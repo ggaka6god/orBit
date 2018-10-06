238c4d34c6c416699416db812f0f90cd731e5b2c
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-10-06 19:25:09 +0200

    old tiled map

---
+++ stage 1/atlas-props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="atlas-props" tilewidth="16" tileheight="16" tilecount="120" columns="6">
+ <image source="../Tilesets/Sunny_Land/atlas-props.png" width="109" height="333"/>
+</tileset>
---
+++ stage 1/atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="atlas" tilewidth="16" tileheight="16" tilecount="240" columns="30">
+ <image source="../Tilesets/Sunny_Land/atlas.png" width="490" height="143"/>
+</tileset>
