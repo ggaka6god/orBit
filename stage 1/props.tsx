30a78aab84ad23838789a91aff43c70f875ab9e7
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-10-06 18:43:54 +0200

    update primer mapa

---
+++ stage 1/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="props - Copy" tilewidth="16" tileheight="16" tilecount="544" columns="34">
+ <image source="../Tilesets/Forest/props - Copy.png" width="544" height="256"/>
+</tileset>
---
+++ stage 1/tilesetsunnyland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tileset order" tilewidth="16" tileheight="16" tilecount="378" columns="18">
+ <image source="../Tilesets/Sunny_Land/tileset order.png" width="288" height="336"/>
+</tileset>
