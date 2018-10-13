2e7d24ca969a4126064a5b77ab309c95b2741197
Andres Saladrigas <36264953+TheArzhel@users.noreply.github.com>
2018-10-13 12:45:48 +0200

    collider issue solved stage
    -colliders miss interpretation in stage 2
    -erase extra unnecesasry collider stage1

--- Dev_class7_handout2.0/Motor2D/j1Scene.cpp
+++ Dev_class7_handout2.0/Motor2D/j1Scene.cpp
@@ -117,10 +117,10 @@ bool j1Scene::Start()
 
 		App->map->ColliderDrawer(App->map->data);
 
-		colliderfloor = App->coll->AddCollider({ 0, 150, 1024, 100 }, COLLIDER_FLOOR, this);
-		colliderbox = App->coll->AddCollider({ 100, 120, 50, 30 }, COLLIDER_FLOOR, this);
-		colliderbox = App->coll->AddCollider({ 151, 120, 50, 30 }, COLLIDER_FLOOR, this);
-		test = App->coll->AddCollider({ 400,50,20,20 }, COLLIDER_FLOOR, this);
+		//colliderfloor = App->coll->AddCollider({ 0, 150, 1024, 100 }, COLLIDER_FLOOR, this);
+		//colliderbox = App->coll->AddCollider({ 100, 120, 50, 30 }, COLLIDER_FLOOR, this);
+		//colliderbox = App->coll->AddCollider({ 151, 120, 50, 30 }, COLLIDER_FLOOR, this);
+		//test = App->coll->AddCollider({ 400,50,20,20 }, COLLIDER_FLOOR, this);
 
 	return ret;
 }
